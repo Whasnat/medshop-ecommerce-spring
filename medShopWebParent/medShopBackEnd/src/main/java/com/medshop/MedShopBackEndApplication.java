43ddf340d1524a7e7c10910b44d39d3b50f5f837
whasnat <w.hasnat94@gmail.com>
2021-11-28 00:57:02 +0600

    App Structure done

---
+++ medShopWebParent/medShopBackEnd/src/main/java/com/medshop/MedShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.medshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedShopBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedShopBackEndApplication.class, args);
+    }
+
+}
